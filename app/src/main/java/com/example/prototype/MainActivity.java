34593f75b65029554d612b29b4db6264a2e27dfe
Brandon Gutierrez <brandon.gutierrez@iut-rodez.fr>
2022-04-12 15:27:01 +0200

    vise vue batiment

--- app/src/main/java/com/example/prototype/MainActivity.java
+++ app/src/main/java/com/example/prototype/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.salles);
+        setContentView(R.layout.batiments);
     }
 }
