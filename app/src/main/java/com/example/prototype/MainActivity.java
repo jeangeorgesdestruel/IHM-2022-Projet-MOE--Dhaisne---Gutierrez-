ac4a23a40b3227be59ef642d00d6b9ddc90d4ec6
Brandon Gutierrez <brandon.gutierrez@iut-rodez.fr>
2022-04-13 09:06:02 +0200

    Autre liaison + menu options

--- app/src/main/java/com/example/prototype/MainActivity.java
+++ app/src/main/java/com/example/prototype/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
@@ -15,8 +18,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.batiments);
 
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        new MenuInflater(this).inflate(R.menu.menu_options, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // selon l'option sélectionnée dans le menu, on réalise le traitement adéquat
+        switch(item.getItemId()) {
+            case R.id.option_mes_reservations :  		 // ajout d'une nouvelle cuisson
+//                saisirAjouterCuisson();
+                break;
+            case R.id.option_retour_accueil:
+//                saisirRechercherAliment();
+                break;
+
+        }
+        return (super.onOptionsItemSelected(item));
+    }
 
     public void afficherEtages(View view) {
         Intent intent = new Intent(getApplicationContext(),Etages.class);
