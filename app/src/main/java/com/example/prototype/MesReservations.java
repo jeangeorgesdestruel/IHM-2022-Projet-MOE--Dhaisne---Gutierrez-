acd747cc1fd9320058f450dfa94b6b56065d9b26
Clement Dhaisne <clement.dhaisne@iut-rodez.fr>
2022-04-13 09:12:56 +0200

    Ajout des derniere vue + remplissage des listes

--- app/src/main/java/com/example/prototype/MesReservations.java
+++ app/src/main/java/com/example/prototype/MesReservations.java
@@ -1,13 +1,30 @@
 package com.example.prototype;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MesReservations extends AppCompatActivity {
+
+    ListView listeMesReservations;
+
+    ArrayAdapter<String> adaptateur;
+
+    String[] resultatReservation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mes_reservations);
+
+        listeMesReservations = (ListView) findViewById(R.id.resultat_recherche);
+
+        resultatReservation = getResources().getStringArray(R.array.mesReserv);
+
+        adaptateur = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, resultatReservation);
+
+        listeMesReservations.setAdapter(adaptateur);
     }
 }
--- app/src/main/java/com/example/prototype/ResultatRecherche.java
+++ app/src/main/java/com/example/prototype/ResultatRecherche.java
@@ -1,14 +1,30 @@
 package com.example.prototype;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ResultatRecherche extends AppCompatActivity {
+
+    ListView listeResultatRecherche;
+
+    ArrayAdapter<String> adaptateur;
+
+    String[] resultatRecherche;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.resultatrech);
 
+        listeResultatRecherche = (ListView) findViewById(R.id.resultat_recherche);
+
+        resultatRecherche = getResources().getStringArray(R.array.resultRecherche);
+
+        adaptateur = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, resultatRecherche);
+
+        listeResultatRecherche.setAdapter(adaptateur);
     }
 }
